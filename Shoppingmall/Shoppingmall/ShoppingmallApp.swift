b061e6ba0bccfc9578606c6991d591cf0e3d7b88
Nikita Pivovarov <nikita.pivovarov2003@gmail.com>
2023-10-02 15:13:53 +0300

    initial

---
+++ Shoppingmall/Shoppingmall/ShoppingmallApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingmallApp.swift
+//  Shoppingmall
+//
+//  Created by Никита Пивоваров on 02.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingmallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
